f3b57ccef5ee6156b17b0c1b99fe5ec775b1766c
Dhamodharan.c <102224582+Dhamuvei@users.noreply.github.com>
2023-02-03 15:39:39 +0530

    Update Auth.js

--- Router/Auth.js
+++ Router/Auth.js
@@ -1,7 +1,7 @@
-const Route = require("express").Router();
-const Auth_Controller = require("../controller/Auth");
+// const Route = require("express").Router();
+// const Auth_Controller = require("../controller/Auth");
 
-//Registration
-Route.post("/register",Auth_Controller.Registration);
-//Login
-Route.post("/login",Auth_Controller.Login)
+// //Registration
+// Route.post("/register",Auth_Controller.Registration);
+// //Login
+// Route.post("/login",Auth_Controller.Login)
